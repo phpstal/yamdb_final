517edfe1c7f1fd59f0f308db3749b79d844e0bb4
Vladimir <vrmy87@yandex.ru>
2021-06-27 00:24:40 +0700

    changed docker file 3

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
